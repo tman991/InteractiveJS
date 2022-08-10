aa4ee4aa9e6a5826d17c9f6a5db6034a1a2b0ca6
Taha Manjoura <t.manjoura@gmail.com>
2022-08-10 17:22:02 -0500

    interactiveJS

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
-console.log('hello world')
 
-function addMovie(event) {
+
+const addMovie = (event) => {
+
+    event.preventDefault()
 
 let inputField = document.querySelector('input')
 
+let movie = document.createElement('li')
 
+let movieTitle = document.createElement("span");
+
+movieTitle.textcontent = inputField.value;
+
+movie.appendChild(movieTitle)
+
+let deleteBtn = document.createElement("button");
+
+deleteBtn.textContent = "x";
+
+deleteBtn.addEventListener("click", deleteMovie);
+
+movie.appendChild(deleteBtn)
+
+document.querySelector("ul").appendChild(movie)
+
+inputField.value = ''
+
+}
+
+const deleteMovie = (event => {
+    event.target.parentNode.remove()
+})
+document.querySelector("form").addEventListener("submit", addMovie);
+
+
+
+
+function crossOffMovie(event) {
+
+    event.target.classList.toggle("checked")
+
+
+        if (event.target.classList.contains('checked') === true) {
+
+            message.textContent = 'Already watched!'
+            
+        }  else {  message.textContent = 'Movie added back!'
+            
+        }
+        
+      
 
 }
